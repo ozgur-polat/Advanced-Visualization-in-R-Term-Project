054b2a582b4be4ed9bbf93e216947808eebcc813
Huseyin Can Minareci <66742896+hcan-m@users.noreply.github.com>
2020-12-22 22:49:21 +0100

    Update Project.R

--- Project.R
+++ Project.R
@@ -2,14 +2,30 @@
 #               Advanced Visualization in R               #
 #              Ozgur POLAT & H. Can Minareci              #
 #             Faculty of Economic Sciences UW             #
-#                   Term Projectenverden                  #
+#                      Term Project                       #
 #---------------------------------------------------------#
 
 # Set the working directory
 setwd('...')
 
 # Read database
-spare.time <- read.csv("Data/churn.csv", sep = ',')
+BankChurners <- read.csv("BankChurners.csv", stringsAsFactors = T)
 
 # Read libs
-head(spare.time)
+head(BankChurners)
+
+
+numbers <-c()
+factors <- c()
+for (i in colnames(BankChurners)){
+  if (is.factor(BankChurners[,i]) == TRUE){
+    print(i)
+    print(unique(BankChurners[,i]))
+    factors <- append(factors, i)} else{
+      numbers <- append(numbers, i)
+    }
+
+}
+
+print(numbers)
+print(factors)
